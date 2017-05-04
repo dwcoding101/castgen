08d10a99b57385d6157c4e46632ee1a33f112fc2
Daniel <dwrunner26@gmail.com>
2017-05-04 12:24:50 +0100

    Added fields to Stat Class.

---
+++ src/main/java/org/gen/cast/stat/Stat.java
@@ -0,0 +1,23 @@
+package org.gen.cast.stat;
+
+/**
+ * Created by Daniel on 04/05/2017.
+ */
+public class Stat {
+
+    // Description of the stat.
+    String description;
+
+    // name holds the Name of the Stat
+    String name;
+
+    // string that is used to generate the stat.
+    String generatedBy;
+
+    // value of stat
+    int value;
+
+    public long generateValue() {
+        return 0;
+    }
+}
